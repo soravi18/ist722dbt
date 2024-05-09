cd7ebb0a0d131c901b95169ce45113dd6f5c175d
Soundarya Ravi <soravi@g.syr.edu>
2024-05-09 22:24:30 +0000

    final commit

--- Vision/models/fact_author_royalty.sql
+++ Vision/models/fact_author_royalty.sql
@@ -1,25 +1,10 @@
-WITH stg_titleauthors AS (
-          SELECT * FROM {{ source('VISIONBOOKS', 'TITLEAUTHORS') }}
-      ),
-stg_authors as 
-(
-    select *
-        
-        
-    from {{ source('VISIONBOOKS','AUTHORS') }}
-),
-stg_title as
-(
-    select *
-       
-    from {{ source('VISIONBOOKS', 'TITLES') }}
-),
-stg as (
-select ta.AU_ID as authorkey,
-sum(t.ROYALTY) as royalty_sum
-from 
-    stg_title t
-join stg_titleauthors ta on ta.TITLE_ID=t.TITLE_ID
-join stg_authors a on ta.AU_ID=a.AU_ID
-group by authorkey
-)
+with
+    stg_titleauthors as (select * from {{ source("VISIONBOOKS", "TITLEAUTHORS") }}),
+    stg_authors as (select * from {{ source("VISIONBOOKS", "AUTHORS") }}),
+    stg_title as (select * from {{ source("VISIONBOOKS", "TITLES") }})
+        select ta.au_id as authorkey, sum(t.royalty) as royalty_sum
+        from stg_title t
+        join stg_titleauthors ta on ta.title_id = t.title_id
+        join stg_authors a on ta.au_id = a.au_id
+        group by authorkey
+    
